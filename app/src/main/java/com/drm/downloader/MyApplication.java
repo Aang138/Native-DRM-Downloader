3f5cc057e179c2707fcb90163c6f5d48860a2ea1
Aang138 <Imranimran157@gmail.com>
2026-07-30 22:45:41 +0530

    Fix FFmpeg and Aria2c import package namespaces for junkfood02 dependency

--- app/src/main/java/com/drm/downloader/MyApplication.java
+++ app/src/main/java/com/drm/downloader/MyApplication.java
@@ -5,8 +5,8 @@ import android.util.Log;
 
 import com.yausername.youtubedl_android.YoutubeDL;
 import com.yausername.youtubedl_android.YoutubeDLException;
-import com.yausername.youtubedl_android.ffmpeg.FFmpeg;
-import com.yausername.youtubedl_android.aria2c.Aria2c;
+import com.github.junkfood02.youtubedl_android.ffmpeg.FFmpeg;
+import com.github.junkfood02.youtubedl_android.aria2c.Aria2c;
 
 public class MyApplication extends Application {
     @Override
